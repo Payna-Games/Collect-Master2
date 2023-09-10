93016e80a698fcc38fb840fe3f96472dbf38228d
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-09-10 17:30:11 +0300

    playerController yazıldı

--- CollectMaster2/Assets/Scripts/PlayerController.cs
+++ CollectMaster2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FloatingJoystick _joystick;
+    //[SerializeField] private Animator _animator;
+   
+    [SerializeField] private float _moveSpeed;
+   
+    private void FixedUpdate()
     {
-        
+        _rigidbody.velocity = new Vector3(-_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, -_joystick.Vertical * _moveSpeed);
+   
+        //if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        // {
+        //     transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+        //_animator.SetBool("isRunning", true);
+        // }
+        // else
+        //  _animator.SetBool("isRunning", false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
