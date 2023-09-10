aee5d9015e6f2471dd12cb3d564433f968a40b42
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-09-10 22:41:31 +0300

    circle Fill amaount eklendi

--- CollectMaster2/Assets/Scripts/HoleSize.cs
+++ CollectMaster2/Assets/Scripts/HoleSize.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.UI;
 
 public class HoleSize : MonoBehaviour
 {
     [SerializeField] private float scaleIncraseThreshold;
     [SerializeField] private float scaleStep;
+    [SerializeField] private Image circle;
     private float scaleValue;
+    private float circleRatio;
     
 
     private void IncreaseScale()
@@ -18,11 +23,15 @@ public class HoleSize : MonoBehaviour
     public void CollectibleCollected(float objectSize)
     {
         scaleValue += objectSize;
+        circleRatio = scaleValue / scaleIncraseThreshold;
+        circle.fillAmount += circleRatio;
         if (scaleValue >= scaleIncraseThreshold)
         {
             IncreaseScale();
             scaleValue = scaleValue % scaleIncraseThreshold;
+            circle.fillAmount = 0;
+
         }
-        Debug.Log("increase the size by " + objectSize);
+        
     }
 }
