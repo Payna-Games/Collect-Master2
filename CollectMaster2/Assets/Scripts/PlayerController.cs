2e02b04810cc27331c65a70e45c6494c08d304cb
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-09-10 17:49:17 +0300

    playerController yazıldı ama sıkıntılı

--- CollectMaster2/Assets/Scripts/PlayerController.cs
+++ CollectMaster2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
+    //[SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private FloatingJoystick _joystick;
-    //[SerializeField] private Animator _animator;
+ 
    
     [SerializeField] private float _moveSpeed;
    
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector3(-_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, -_joystick.Vertical * _moveSpeed);
+         transform.Translate(new Vector3(_joystick.Horizontal * _moveSpeed, 0f ,_joystick.Vertical * _moveSpeed));
    
         //if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
         // {
