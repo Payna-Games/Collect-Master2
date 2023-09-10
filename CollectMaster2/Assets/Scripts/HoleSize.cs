79c05094e859dca46de2fbe382a56ead24766e98
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-09-10 19:40:54 +0300

    holeScale artışı yapıldı

--- CollectMaster2/Assets/Scripts/HoleSize.cs
+++ CollectMaster2/Assets/Scripts/HoleSize.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class HoleSize : MonoBehaviour
 {
+    [SerializeField] private float scaleIncraseThreshold;
+    [SerializeField] private float scaleStep;
+    private float scaleValue;
+    
+
+    private void IncreaseScale()
+    {
+        transform.localScale += scaleStep * Vector3.one;
+        
+    }
+    
     public void CollectibleCollected(float objectSize)
     {
+        scaleValue += objectSize;
+        if (scaleValue >= scaleIncraseThreshold)
+        {
+            IncreaseScale();
+            scaleValue = scaleValue % scaleIncraseThreshold;
+        }
         Debug.Log("increase the size by " + objectSize);
     }
 }
